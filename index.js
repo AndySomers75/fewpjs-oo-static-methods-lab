6120b63dedf623ed8a9ddd3f2d1a5b6cf582633c
Andrew <andrew@pureaquasprings.com>
2022-04-04 23:23:22 +1000

    commit tree 6a426468a0f0720e3354811a38111cc044cf299e

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string){
+    this.string = 'crocodile';
+    return string.charAt( 0).toUpperCase() + string.slice ( 1 )
+  }
+
+  static sanitize(string) {
+    return string.replace("ca$@#tching cold" + "Doin Dishes" + "Entert*ain(i{ng-Elphan^ts" + "!");
+   
+  }
+};
